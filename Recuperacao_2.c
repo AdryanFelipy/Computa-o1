43b03556ac9faa5c58901e61ba7109f829d6b1ab
AdryanFelipy <adryanfelipy@alunos.utfpr.edu.br>
2025-04-24 16:07:53 -0300

    Create Recuperacao_2.c

---
+++ Recuperacao_2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int base, inicio, fim, soma = 0, i;
+
+    printf("Digite o número base: ");
+    scanf("%d", &base);
+    printf("Digite o valor inicial do intervalo: ");
+    scanf("%d", &inicio);
+    printf("Digite o valor final do intervalo: ");
+    scanf("%d", &fim);
+
+    i = inicio;
+    while (i <= fim) {
+        if (i % base == 0) {
+            soma += i;
+        }
+        i++;
+    }
+
+    printf("Soma dos múltiplos de %d entre %d e %d: %d\n", base, inicio, fim, soma);
+
+    return 0;
+}
